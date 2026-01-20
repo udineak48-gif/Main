9b1881fa74b2561d4a51e5683a23f8b9f2ccc0ee
udineak48-gif <udineak48@gmail.com>
2026-01-20 13:13:05 +0700

    Add files via upload

---
+++ mt.lua
@@ -0,0 +1,49 @@
+-- mount_config.lua
+
+-- Konfigurasi Mounts
+---------------------------------------------------------------------
+local FloatingMounts = {
+    ["MOUNT YAHAYUK"] = { 
+    v1 = "json/mount_yahayuk_normal.json", 
+    v2 = "",
+    v3 = "" },
+    
+     
+    
+         ["MOUNT POSWA"] = { 
+    v1 = "json/mount_poswa.json", 
+    v2 = "",
+    v3 = "" }
+    
+
+
+}
+
+-- Daftar Mount yang pakai Checkpoint Toggle (List)
+
+local CheckpointMounts = {}
+-- [[ local CheckpointMounts = {
+   -- ["MOUNT VELORA"] = {
+       -- path = "mount_velora/",
+      --  files = {} 
+ --   }, 
+   
+--;}
+
+-- Generate File List
+
+-- local veloraFiles = CheckpointMounts["MOUNT VELORA"].files
+-- table.insert(veloraFiles, { name = "Spawnpoint", file = "spawnpoint.json" })
+-- for i = 1, 51 do
+   --  table.insert(veloraFiles, { 
+       -- name = "Checkpoint " .. i, 
+      --  file = "checkpoint_" .. i .. ".json" 
+   -- })
+-- end
+
+
+
+return {
+    FloatingMounts = FloatingMounts,
+    CheckpointMounts = CheckpointMounts
+}

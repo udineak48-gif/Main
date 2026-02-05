d16fd61ea0c9263297bab2553cfa8fac50d27d3a
udineak48-gif <udineak48@gmail.com>
2026-02-06 01:16:41 +0700

    Update mt.lua

--- mt.lua
+++ mt.lua
@@ -8,11 +8,55 @@ local FloatingMounts = {
     v2 = "",
     v3 = "" },
    
-    ["MT YNTKS"] = { 
-    v1 = "mount_yntks.json", 
+    ["MT YAGESYA"] = { 
+    v1 = "mount_yagesya.json", 
     v2 = "",
     v3 = "" },
 
+      ["MT MUKJIZAT"] = { 
+    v1 = "mount_mukjizat.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT KITA"] = { 
+    v1 = "mount_kita.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT FUNNY"] = { 
+    v1 = "mount_funny.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT FREESTYLE"] = { 
+    v1 = "mount_freestyle.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT DAUN"] = { 
+    v1 = "mount_daun.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT BEJIRLAH"] = { 
+    v1 = "mount_bejirlah.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT AYRIENE"] = { 
+    v1 = "mount_ayriene.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT AGEE"] = { 
+    v1 = "mount_agee.json", 
+    v2 = "",
+    v3 = "" },
+
+      ["MT YAYAKIN"] = { 
+    v1 = "mount_yayakin.json", 
+    v2 = "",
+    v3 = "" },
 
          ["MT POSWA"] = { 
     v1 = "mount_poswa.json", 
@@ -51,5 +95,6 @@ return {
     FloatingMounts = FloatingMounts,
     CheckpointMounts = CheckpointMounts
 }
+
 
 

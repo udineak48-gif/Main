151fd998cacac19f1b2e04870b3f2f3843160011
udineak48-gif <udineak48@gmail.com>
2026-03-23 09:35:25 +0700

    Update mt.lua

--- mt.lua
+++ mt.lua
@@ -3,61 +3,16 @@
 -- Konfigurasi Mounts
 ---------------------------------------------------------------------
 local FloatingMounts = {
-    ["MT YAHAYUK V3"] = { 
-    v1 = "mount_yahayuk_v3.json", 
+    ["MT NOXERA"] = { 
+    v1 = "mount_noxera.json", 
     v2 = "",
     v3 = "" },
    
-    ["MT YAGESYA"] = { 
-    v1 = "mount_yagesya.json", 
+    ["MT ASTERA"] = { 
+    v1 = "mount_astera.json", 
     v2 = "",
     v3 = "" },
-
-      ["MT MUKJIZAT"] = { 
-    v1 = "mount_mukjizat.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT KITA"] = { 
-    v1 = "mount_kita.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT FUNNY"] = { 
-    v1 = "mount_funny.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT FREESTYLE"] = { 
-    v1 = "mount_freestyle.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT DAUN"] = { 
-    v1 = "mount_daun.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT BEJIRLAH"] = { 
-    v1 = "mount_bejirlah.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT AYRIENE"] = { 
-    v1 = "mount_ayriene.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT AGEE"] = { 
-    v1 = "mount_agee.json", 
-    v2 = "",
-    v3 = "" },
-
-      ["MT YAYAKIN"] = { 
-    v1 = "mount_yayakin.json", 
-    v2 = "",
-    v3 = "" },
-
+    
          ["MT POSWA"] = { 
     v1 = "mount_poswa.json", 
     v2 = "",
